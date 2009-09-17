96ccabdf8a320cf142f84ccdaeccdb724a9124c0
Jordi Romero <jordi@jrom.net>
2009-09-17 13:52:22 +0200

    Adding queries & time only for the logged in visitors

--- footer.php
+++ footer.php
@@ -5,7 +5,9 @@
       <?php get_sidebar(); ?>
     </div><!-- #sidebar -->
     <div class="clear"> </div>
-    
+    <?php if ( is_user_logged_in() ): ?> 
+    <p><?php echo get_num_queries(); ?> queries en <?php timer_stop(1); ?> segundos</p>
+    <?php endif; ?>
   </div><!-- .wrap -->
 <?php wp_footer(); ?>
 </body>
